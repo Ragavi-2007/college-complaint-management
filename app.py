7596417b72e2150963ab32e9a5a1358eea553af6
Ragavi-2007 <ragaviragavisasikumar@gmail.com>
2026-09-14 21:29:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -591,6 +591,7 @@ def file_too_large(error):
     return redirect(url_for("submit_complaint"))
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     app.run(debug=True, host="0.0.0.0", port=5000)
